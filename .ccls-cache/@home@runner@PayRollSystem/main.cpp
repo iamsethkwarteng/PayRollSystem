177e5228081e219ae6ffa0671bead95c2c806b59
iamsethkwarteng <157321622+iamsethkwarteng@users.noreply.github.com>
2024-07-03 12:26:53 +0000

    ddddd

--- .ccls-cache/@home@runner@PayRollSystem/main.cpp
+++ .ccls-cache/@home@runner@PayRollSystem/main.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
 int main() {
-    double hours, rate, gross, tax, net;
+  double hours, rate, gross, tax, net;
 
-    cout << "Enter hours worked: ";
-    cin >> hours;
+  cout << "Enter hours worked: ";
+  cin >> hours;
 
-    cout << "Enter hourly rate: ";
-    cin >> rate;
+  cout << "Enter hourly rate: ";
+  cin >> rate;
 
-    gross = hours * rate;
+  gross = hours * rate;
 
-    tax = gross * 0.15;
+  tax = gross * 0.15;
 
-    net = gross - tax;
+  net = gross - tax;
 
-    cout << fixed << setprecision(2);
+  cout << fixed << setprecision(2);
 
-    cout << "\nGross Pay: $" << gross << endl;
-    cout << "Taxes: $" << tax << endl;
-    cout << "Net Pay: $" << net << endl;
+  cout << "\nGross Pay: dfvfv$" << gross << endl;
+  cout << "Taxes: $" << tax << endl;
+  cout << "Net Pay: $" << net << endl;
 
-    return 0;
+  return 0;
 }
